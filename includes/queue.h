900e9a9b9bc840e6bf45483a3d7439d30083c312
Sergey Malyshev <in1t@ya.ru>
2020-06-25 21:57:19 +0300

    a

--- includes/queue.h
+++ includes/queue.h
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 21:05:12 by bconchit          #+#    #+#             */
-/*   Updated: 2020/06/25 21:47:42 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/06/25 21:56:52 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,26 @@
 typedef struct s_queue_item	t_queue_item;
 typedef struct s_queue		t_queue;
 
-struct	s_queue_item
+struct			s_queue_item
 {
 	t_queue_item	*head;
 	t_queue_item	*tail;
 };
 
-struct	s_queue
+struct			s_queue
 {
 	size_t	size;
 	void	*head;
 	void	*tail;
 };
 
-t_queue	*queue_create(void);
-void	queue_destroy(t_queue **aself);
-void	queue_clean(t_queue *self);
-void	*queue_pop_front(t_queue *self);
-void	queue_push_back(t_queue *self, void *data);
+t_queue_item	*queue_item_create(void);
+void			queue_item_destroy(t_queue_item **aself);
+
+t_queue			*queue_create(void);
+void			queue_destroy(t_queue **aself);
+void			queue_clean(t_queue *self);
+void			*queue_pop_front(t_queue *self);
+void			queue_push_back(t_queue *self, void *data);
 
 #endif
---
+++ srcs/queue/queue_item_create.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue_item_create.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bconchit <bconchit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 21:50:16 by bconchit          #+#    #+#             */
+/*   Updated: 2020/06/25 21:51:11 by bconchit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "queue.h"
+
+t_queue_item	*queue_item_create(void)
+{
+	t_queue_item		*self;
+
+	self = (t_queue_item *)ft_xmemalloc(sizeof(t_queue_item));
+	return (self);
+}

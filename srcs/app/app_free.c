5590afaf59ec1079ef356dac58ed7450b21c71af
in1t <in1t@ya.ru>
2020-06-27 15:27:22 +0300

    a

--- srcs/app/app_free.c
+++ srcs/app/app_free.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 21:49:29 by bconchit          #+#    #+#             */
-/*   Updated: 2020/06/27 14:56:14 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/06/27 15:10:32 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,8 @@
 void	app_free(t_app *self)
 {
 	//ft_printf("FREE\n");
-	if (self->gnl)
-		gnl_destroy(&self->gnl);
-	if (self->lines)
-	{
-		while ((self->line = queue_pop_front(self->lines)))
-			ft_strdel(&self->line);
-		queue_destroy(&self->lines);
-	}
+	gnl_destroy(&self->gnl);
+	while ((self->line = queue_pop_front(self->lines)))
+		ft_strdel(&self->line);
+	queue_destroy(&self->lines);
 }

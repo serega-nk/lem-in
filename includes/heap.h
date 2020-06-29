dedb4d1744fbce9bbfa7f2c971378e42a3b7586a
Sergey Malyshev <in1t@ya.ru>
2020-06-29 18:28:45 +0300

    a

--- includes/heap.h
+++ includes/heap.h
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 14:04:45 by bconchit          #+#    #+#             */
-/*   Updated: 2020/06/29 18:23:08 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/06/29 18:28:20 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "ft_xexit.h"
 # include "ft_printf.h"
 
-typedef struct t_heap	t_heap;
+typedef struct s_heap	t_heap;
 
 struct		s_heap
 {

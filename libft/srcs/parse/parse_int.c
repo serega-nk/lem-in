328235e438b74c72090f186e0bb564d0b986bb45
Sergey Malyshev <in1t@ya.ru>
2020-07-11 21:44:22 +0300

    a

--- libft/srcs/parse/parse_int.c
+++ libft/srcs/parse/parse_int.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/27 02:44:31 by bconchit          #+#    #+#             */
-/*   Updated: 2020/06/29 05:09:36 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/07/11 21:42:37 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 int		parse_int(char **addr, int *avalue)
 {
 	char	*ptr;
-	long	prev;
-	long	curr;
+	long	value;
 	int		negative;
 
 	ptr = *addr;
@@ -27,16 +26,16 @@ int		parse_int(char **addr, int *avalue)
 	{
 		if (*ptr == '0' && ft_isdigit(*(ptr + 1)))
 			return (0);
-		curr = 0;
+		value = 0;
 		while (ft_isdigit(*ptr))
 		{
-			prev = curr;
-			curr = curr * 10 + (*ptr++ - '0');
-			if ((int)(curr - negative) < (int)(prev - negative))
+			value = value * 10 + (*ptr++ - '0');
+			if ((int)(value - negative) != (value - negative))
 				return (0);
 		}
-		*avalue = (int)(curr * (negative ? -1 : 1));
-		return ((*addr = ptr) || 1);
+		*addr = ptr;
+		*avalue = (int)(value * (negative ? -1 : 1));
+		return (1);
 	}
 	return (0);
 }

ed4ec1d5ea4303257978f0bc5acdbe63c14afd80
in1t <in1t@ya.ru>
2020-07-19 17:07:52 +0300

    ft_xmemdup

--- libft/srcs/ft_xexit/ft_xmemdup.c
+++ libft/srcs/ft_xexit/ft_xmemdup.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_xexit.h                                         :+:      :+:    :+:   */
+/*   ft_xmemdup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/16 02:50:47 by bconchit          #+#    #+#             */
-/*   Updated: 2020/07/19 17:02:30 by bconchit         ###   ########.fr       */
+/*   Created: 2020/07/19 16:58:55 by bconchit          #+#    #+#             */
+/*   Updated: 2020/07/19 17:02:12 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_XEXIT_H
-# define FT_XEXIT_H
+#include "ft_xexit_private.h"
 
-# include <stdlib.h>
-
-void	ft_on_xexit(void (*function)(), void *arg);
-void	ft_xexit(int status);
-void	*ft_xmalloc(size_t size);
-void	*ft_xmemalloc(size_t size);
-void	*ft_xcheck(void *ptr);
-char	*ft_xstrdup(const char *s1);
-char	*ft_xstrdup2(const char *s1, const char *s2);
-void	*ft_xmemdup(const void *src, size_t size);
-
-#endif
+void	*ft_xmemdup(const void *src, size_t size)
+{
+	return (ft_memcpy(ft_xmalloc(size), src, size));
+}

fcca7d8bddb1c8fde00abc6b8a8c59149391bee7
in1t <in1t@ya.ru>
2020-07-19 17:23:04 +0300

    fix gnl_destroy Segmentation fault

--- libft/srcs/gnl/gnl_destroy.c
+++ libft/srcs/gnl/gnl_destroy.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 22:30:06 by bconchit          #+#    #+#             */
-/*   Updated: 2020/06/27 13:43:21 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/07/19 17:21:08 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ void	gnl_destroy(t_gnl **aself)
 {
 	if (aself && *aself)
 	{
-		while ((*aself)->save[0])
+		if ((*aself)->save)
 		{
-			ft_memdel((void **)&(*aself)->save[0]);
-			(*aself)->save++;
+			while ((*aself)->save[0])
+			{
+				ft_memdel((void **)&(*aself)->save[0]);
+				(*aself)->save++;
+			}
 		}
 		ft_memdel((void **)&(*aself)->addr);
 		ft_memdel((void **)aself);

99b3c6df5d7f4b50c6db2624034fcaab9bf95e47
in1t <in1t@ya.ru>
2020-07-25 00:41:57 +0300

    libft fix bug len ft_xstrdup3

--- libft/srcs/ft_xexit/ft_xstrdup3.c
+++ libft/srcs/ft_xexit/ft_xstrdup3.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 04:21:24 by bconchit          #+#    #+#             */
-/*   Updated: 2020/07/21 04:22:22 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/07/25 00:40:59 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char		*ft_xstrdup3(const char *s1, const char *s2, const char *s3)
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	len3 = ft_strlen(s3);
-	res = (char *)ft_xcheck((void *)ft_strnew(len1 + len2 + len2));
+	res = (char *)ft_xcheck((void *)ft_strnew(len1 + len2 + len3));
 	ft_strncpy(res, s1, len1);
 	ft_strncpy(res + len1, s2, len2);
 	ft_strncpy(res + len1 + len2, s3, len3);

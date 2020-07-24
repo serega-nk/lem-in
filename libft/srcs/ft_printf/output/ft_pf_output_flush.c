b3aeb80ba366badc86bac847fdde75c683bcd77b
Sergey Malyshev <in1t@ya.ru>
2020-07-24 17:33:29 +0300

    ft_sprintf fix bug \0 end

--- libft/srcs/ft_printf/output/ft_pf_output_flush.c
+++ libft/srcs/ft_printf/output/ft_pf_output_flush.c
@@ -6,7 +6,7 @@
 /*   By: bconchit <bconchit@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 03:12:47 by bconchit          #+#    #+#             */
-/*   Updated: 2019/11/23 20:39:30 by bconchit         ###   ########.fr       */
+/*   Updated: 2020/07/24 17:31:36 by bconchit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ void	ft_pf_output_flush(t_parse *self)
 
 	if (self->count > 0)
 	{
-		if (self->fd == -1)
+		if (self->str)
 		{
-			if (self->str)
-				ft_memcpy(self->str + self->result, self->buff, self->count);
-			ret = self->count;
+			ft_memcpy(self->str + self->result, self->buff, self->count);
+			self->result += self->count;
+			self->str[self->result] = '\0';
 		}
+		else if (self->fd == -1)
+			self->result += self->count;
 		else
+		{
 			ret = write(self->fd, self->buff, self->count);
-		if (ret > 0)
-			self->result += (int)ret;
+			if (ret > 0)
+				self->result += (int)ret;
+		}
 	}
 	self->count = 0;
 }
